d928e91303534bac138d6f34a420cc16193ec85d
Otávio Augusto <35488608+otavioacb@users.noreply.github.com>
2026-02-16 15:25:27 -0300

    Restore UART HAL header and implementation

--- firmware/hal/uart/uart-hal.c
+++ firmware/hal/uart/uart-hal.c
@@ -50,26 +50,20 @@
 
 void HAL_UART_Init(unsigned int baud_rate)
 {
-	// Calculating clock frequency
 	unsigned int ubrr = (F_CPU / (16UL * baud_rate)) - 1;
 
-	// Upload register values for UBRRn (12-bits)
 	UBRR0H = (unsigned char) (ubrr >> 8);
 	UBRR0L = (unsigned char) ubrr;
 	
-	// Enabling receiver and transmitter
 	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
 	
-	// Set Frame format: 8 data and 2 stop bits
 	UCSR0C = (1 << USBS0) | (3 << UCSZ00);
 }
 
 void HAL_UART_Transmit(unsigned char data)
 {
-	// Waiting for empty transmit buffer
 	while(!(UCSR0A & (1 << UDRE0)));
-	
-	// Copying the data to be transmitted
+
 	UDR0 = data;
 }
 
@@ -77,5 +71,6 @@ void HAL_UART_Send(unsigned char* msg, size_t len_msg)
 {
 	for(int i = 0; i < len_msg; ++i) HAL_UART_Transmit(msg[i]);
 }
+
 
 
